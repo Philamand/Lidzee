24d10d755c02b41efbfc8439131b9caffead01f5
Philamand <contact@autobacklog.com>
2025-12-02 15:34:41 +0000

    Updated prod settings

--- onceuponatime/settings.py
+++ onceuponatime/settings.py
@@ -20,14 +20,12 @@ ALLOWED_HOSTS: List[str] = os.getenv(
 # APPLICATION DEFINITION
 # ─────────────────────────────────────────────────────────────
 INSTALLED_APPS = [
-    # Django
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Local
     "book.apps.BookConfig",
 ]
 
@@ -112,7 +110,6 @@ USE_TZ = True
 # ─────────────────────────────────────────────────────────────
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
@@ -136,11 +133,13 @@ CACHES = {
 # PRODUCTION
 # ─────────────────────────────────────────────────────────────
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    CSRF_TRUSTED_ORIGINS = [
+        os.getenv("CSRF_TRUSTED_ORIGINS"),
+    ]
     SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = "DENY"
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
