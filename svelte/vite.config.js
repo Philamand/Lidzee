561ebd0d34c4fdfd647911a35bed01e2f4556758
Philamand <contact@autobacklog.com>
2025-12-04 16:17:57 +0000

    Added tailwind

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -1,20 +1,25 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    tailwindcss(),
+  ],
   build: {
-    outDir: resolve(__dirname, '../static/svelte/'),
+    outDir: resolve(__dirname, '../static/'),
     emptyOutDir: false,
     rollupOptions: {
       input: {
         'book_detail': './src/book_detail.js',
+        'styles': './src/style.css'
       },
       output: {
-        entryFileNames: '[name]-[hash].js',
-        chunkFileNames: '[name]-[hash].js',
+        entryFileNames: 'svelte/[name]-[hash].js',
+        chunkFileNames: 'svelte/[name]-[hash].js',
         assetFileNames: '[name].[ext]'
       }
     }
