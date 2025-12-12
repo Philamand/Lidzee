5151722d31d58b3b9577254b70cfd0eb4c8fc6a6
Philamand <contact@autobacklog.com>
2025-12-12 12:10:49 +0000

    Removed documentary model and urls

--- documentary/admin.py
+++ documentary/admin.py
@@ -1,11 +1,1 @@
 from django.contrib import admin
-from .models import Documentary
-from book.widgets import AudioRecorderWidget
-
-
-@admin.register(Documentary)
-class DocumentaryModelAdmin(admin.ModelAdmin):
-    def formfield_for_dbfield(self, db_field, request, **kwargs):
-        if db_field.name == "audio":
-            kwargs["widget"] = AudioRecorderWidget
-        return super().formfield_for_dbfield(db_field, request, **kwargs)
--- documentary/views.py
+++ documentary/views.py
@@ -1,19 +1,1 @@
 from typing import Any
-from django.views.generic import ListView
-from django.db.models.query import QuerySet
-from .models import Documentary
-
-
-class DocumentaryListView(ListView):
-    model = Documentary
-    context_object_name = "documentary_list"
-    ordering = "title"
-
-    def get_queryset(self) -> QuerySet[Documentary]:
-        return super().get_queryset().filter(published=True)
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context: dict[str, Any] = super().get_context_data(**kwargs)
-        context["disable_autoplay"] = True
-
-        return context
