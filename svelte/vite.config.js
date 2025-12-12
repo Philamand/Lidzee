3410b5acbb8165dc42f10013f56954de0d0babab
Philamand <contact@autobacklog.com>
2025-12-12 22:38:18 +0000

    Updated vite config

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -15,10 +15,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         'book_detail': './src/book_detail.js',
-        'documentary_list': './src/documentary_list.js',
         'htmx': './src/htmx.js',
         'dock': './src/dock.js',
-        'swiper': './src/swiper.js',
         'styles': './src/style.css'
       },
       output: {
